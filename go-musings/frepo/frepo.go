4f2e9442e0c0db025f108c47e358f78227f6b999
Carlos Martinez <cmm@cagnazzo.me>
2017-11-06 23:14:20 -0300

    commit

--- go-musings/frepo/frepo.go
+++ go-musings/frepo/frepo.go
@@ -5,29 +5,46 @@ import "net/http"
 import "io/ioutil"
 import "flag"
 
-func getFile( url string ) []byte {
+type Profile struct {
+	basedir string
+	fileUrl string
+	nameTemplate string
+	refresh int
+}
+
+func getFile( wp Profile ) []byte {
 	// resp, err := http.Get("https://rdap.lacnic.net/rdap/ip/200.7.84.1")
-	resp, err := http.Get(url)
+	resp, err := http.Get( "https://rdap.lacnic.net/rdap/ip/" + wp.nameTemplate )
     if err != nil {
         // handle error
-        fmt.Println("An error has ocurred!")
+		fmt.Println("An error has ocurred!")
+		return False 
     }
     defer resp.Body.Close()
     body, err := ioutil.ReadAll(resp.Body)    
 	return body
 }
 
+func parseProfile() Profile {
+	return Profile{".", "200.40.0.1", "whois-200.40.0.1.json", 86400}
+}
+
 func main() {
 	fmt.Println("FREPO: file repository builder and manager")
 	
 	// parse command line args
-	var profPtr = flag.String("prof", "1.2.3.4", "profile file name")
+	var profPtr = flag.String("prof", "default_profile.json", "profile file name")
 	flag.Parse()
 	var profName = *profPtr
 	fmt.Println("Profile name: "+profName)
 
+	// load and parse profile
+	var wProfile Profile = parseProfile();
+
+	// update file repository
+
 	var b []byte
-	b = getFile( "https://rdap.lacnic.net/rdap/ip/" + profName )
+	b = getFile( wProfile )
 	
     fmt.Println(string(b))
 }
