5f332361a0325c650431323abd10c28a0d410226
Carlos M. Martinez <carlos@xt6.us>
2019-04-11 23:34:55 -0300

    converted to Fire cli

--- apps/30-routingtable_cc/get_rt_cc.py
+++ apps/30-routingtable_cc/get_rt_cc.py
@@ -1,15 +1,21 @@
+#!/usr/bin/env python
 ##############
-#
-#
+# (c) CarlosM carlos@xt6labs.io
+# 
+# 2019-04-09
 ##############
 
 import pytricia
 import sqlite3
 import logging
+import fire
+import collections
 
 dbfile = "data/netdata-20190325.db"
 cc = 'BR'
 type = 'ipv6'
+
+stats = collections.Counter()
 
 class netdatadb:
 	def __init__(self, wdb):
@@ -28,13 +34,19 @@ class netdatadb:
 	# end runsql
 # end netdatadb
 
-if __name__ == "__main__":
-	ndb = netdatadb(dbfile)
+def main(cc, type='ipv6', dbfile='data/netdata-20190325.db', verbose='0'):
+	ndb = netdatadb(dbfile) 
 	pyt = pytricia.PyTricia(48)
 
-	logging.basicConfig(level=logging.INFO)
+	if verbose == 1:
+		logging.basicConfig(filename='rt.log', filemode='w', level=logging.DEBUG)
+	elif verbose == 0: 
+		logging.basicConfig(level=logging.INFO)
+	else:
+		logging.basicConfig(level=logging.INFO)
 
 	logging.info("Loading {} resources into PyTricia".format(cc))
+	logging.info("Looking for {} routes into PyTricia".format(type))
 	sql_cc_pfx = "SELECT * FROM numres WHERE cc='{}' AND type='{}' ". \
 					format(cc, type)
 	for x in ndb.runsql(sql_cc_pfx):
@@ -43,20 +55,25 @@ if __name__ == "__main__":
 		pyt[pfx] = cc
 
 	# go over routing table and get VE routes
-	cnt_routes = 0
-	tot_routes = 0
+	stats = collections.Counter({'total': 0, 'ccroutes': 0})
 	sql_routes = "SELECT * FROM riswhois WHERE type='{}' ".format(type)
 	for x in ndb.runsql(sql_routes):
 		rpfx = str(x['prefix'])
 		rcc = pyt.get(rpfx, None)
-		tot_routes = tot_routes + 1
+		stats['total'] += 1
 		if rcc == cc:
-			logging.info("Found {} route: {}".format(cc, rpfx))
-			cnt_routes = cnt_routes + 1
+			covering_pfx = pyt.get_key(rpfx)
+			logging.info("Found {} route: {}, under allocation {}".format(cc, rpfx, covering_pfx))
+			stats['ccroutes'] += 1
 		# end if
 	# end for
 
-	logging.info("Found {} {} routes".format(cnt_routes, cc))
-	logging.info("Total routes: {}".format(tot_routes))
+	logging.info("Found {} {} routes for type {}". \
+		format(stats['ccroutes'], cc, type))
+	logging.info("Total routes: {}".format(stats['total']))
+## end main
 
+if __name__ == "__main__":
+	fire.Fire(main)
+	
 # END
