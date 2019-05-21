b486b75cd98eaa70964c746179d28b9956784f64
Carlos M. Martinez <carlos@xt6labs.io>
2019-05-20 23:03:05 -0300

    commit

--- apps/30-invalid-rpki-routes/invalid-rpki-routes.py
+++ apps/30-invalid-rpki-routes/invalid-rpki-routes.py
@@ -51,9 +51,36 @@ class counters:
 # end counters
 
 # begin assignValidityStatus
-def assignValidityStatus(wroas, wpfx):
-    vs = "valid"
-    
+def assignValidityStatus(wpyt, wpfx):
+    """
+    wpyt is the pytricia instance
+    wpfx is the prefix to be evaluated
+    """
+    vs = "notfound"
+
+    rpfx = str(wpfx['prefix'])
+    roas = wpyt.get( rpfx, None)
+
+    if roas == None:
+        logging.debug("prefix {} has ROV status NOT_FOUND".format( rpfx ))
+        vs = "notfound"
+        return vs
+
+    # print(roas)
+    for roa in roas:
+        # print(roa)
+        logging.debug("prefix {} has covering ROA ({}, {})" \
+            .format( rpfx, roa['prefix'], roa['origin_as2'] ))
+        if roa['origin_as2'] == wpfx['origin_as']: 
+            logging.debug("prefix {} id VALIDated by ROA, rt_as={}, roa_as={}, roa_pfx={}" \
+                .format( rpfx, x['origin_as'], roa['origin_as2'], roa['prefix'] ) )
+            vs = "valid"
+            break
+        else:
+            logging.debug("prefix {} can be INVALIDated by ROA, rt_as={}, roa_as={}, roa_pfx={}" \
+                .format( rpfx, x['origin_as'], roa['origin_as2'], roa['prefix'] ) )
+            vs = "invalid"
+
     return vs
 # end assignValidityStatus
 
@@ -75,9 +102,11 @@ if __name__ == "__main__":
         pfx = str(e['prefix'])
         logging.debug("loading ROA for {} origin {}".format(pfx, e['origin_as']))
         if roadata_pyt.get(pfx, None) == None:
-            roadata_pyt[pfx] = [e]
+            roadata_pyt[pfx] = [ dict(e) ]
+            # print( dict(e) )
         else:
-            roadata_pyt[pfx].append(e)
+            roadata_pyt[pfx].append( dict(e) )
+            # print( dict(e) )
         stats.inc('nroas')
     #
     logging.info("Loaded {} ROAS into trie".format(stats.get('nroas')) )
@@ -87,36 +116,20 @@ if __name__ == "__main__":
 
     stats.set('ninvalid', 0)
     stats.set('nvalid', 0)
+    stats.set('notfound', 0)
     for x in ndb.runsql("SELECT * FROM riswhois WHERE type='{}' AND pfxlen <=24 ".format(type)):
         rpfx = str(x['prefix'])
-        roas = roadata_pyt.get(rpfx, None)
-        if roas == None:
-            logging.debug("prefix {} has ROV status NOT_FOUND".format( rpfx ))
-        else:
-            rov_status = assignValidityStatus(roas, rpfx)
+        rov_status = assignValidityStatus(roadata_pyt, dict(x) )
 
-            if rov_status == "valid":
-                    # logging.debug("prefix {} has ROV status VALID, rt_as={}, roa_as={}, roa_pfx={}" \
-                    #     .format( rpfx, x['origin_as'], roa['origin_as2'], roa['prefix'] ) )
-                    stats.inc('nvalid')
-            elif rov_status == "invalid":
-                    # logging.info("prefix {} has ROV status INVALID, rt_as={}, roa_as={}, roa_pfx={}" \
-                    #     .format( rpfx, x['origin_as'], roa['origin_as2'], roa['prefix'] ) )
-                    stats.inc('ninvalid')
-
-
-            # for roa in roas:
-            #     # print(roa)
-            #     logging.debug("prefix {} has covering ROA ({}, {})" \
-            #         .format( rpfx, roa['prefix'], roa['origin_as2'] ))
-            #     if roa['origin_as2'] == x['origin_as']: 
-            #         logging.debug("prefix {} has ROV status VALID, rt_as={}, roa_as={}, roa_pfx={}" \
-            #             .format( rpfx, x['origin_as'], roa['origin_as2'], roa['prefix'] ) )
-            #         stats.inc('nvalid')
-            #     else:
-            #         logging.info("prefix {} has ROV status INVALID, rt_as={}, roa_as={}, roa_pfx={}" \
-            #             .format( rpfx, x['origin_as'], roa['origin_as2'], roa['prefix'] ) )
-            #         stats.inc('ninvalid')
+        if rov_status == "valid":
+            stats.inc('nvalid')
+        elif rov_status == "invalid":
+            logging.info("prefix {} has ROV status INVALID, rt_as={}, roa_as={}, roa_pfx={}" \
+                .format( rpfx, x['origin_as'], 'xx', 'yy' ) )
+            stats.inc('ninvalid')
+        elif rov_status == "notfound":
+            stats.inc('notfound')
+    # end for
 
     logging.info("Found {} valid routes".format(stats.get('nvalid')) )
     logging.info("Found {} invalid routes".format(stats.get('ninvalid')) )
